1a172cb55bebe54c117f8c5f8cd83bd7dd4d61b3
Phil Culling <philculling@hotmail.com>
2023-01-22 13:34:40 +0000

    add questions

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -9,7 +9,8 @@ var formEl = document.getElementById("choices");
 //formEl is now the choices part in the index.html
 var userChoice = document.getElementById("option");
 /*userChoice, when selected later in the playGame function, 
-should now link to the option section of the form*/
+should now link to the option section of the form
+which will be created later*/
 var start = document.getElementById("start");
 //start now linked to the button so can now be used in eventListener
 
@@ -25,23 +26,47 @@ var timer = 75;
 
 var questions = [
   {
-      question: "The type of bracket used after the word if in an if statement is?",
-      correctAnswer: "parentheses, as in: (",
-      wrongAnswer: "square brackets, as in [",
+      question: "Commonly used data types do NOT include:",
+      correctAnswer: "alerts",
+      wrongAnswer1: "Booleans",
+      wrongAnswer2: "Strings",
+      wrongAnswer3: "numbers",
   },
   {
-      question: "A function within an object is called?",
-      correctAnswer: "a method",
-      wrongAnswer: "a sub-function",
+      question: "The condition in an if / else statement is enclosed with _____",
+      correctAnswer: "parentheses",
+      wrongAnswer1: "quotes",
+      wrongAnswer2: "curly brackets",
+      wrongAnswer3: "square brackets",
   },
   {
-      question: "Arrays in JavaScript can be used to store...?",
-      correctAnswer: "other arrays",
-      wrongAnswer: "html",
-  }//Add more later, make this 5 questions with 4 choices each
+      question: "Arrays in JavaScript can be used to store_____",
+      correctAnswer: "all of the above",
+      wrongAnswer1: "numbers and strings",
+      wrongAnswer2: "other arrays",
+      wrongAnswer3: "booleans",
+  },
+/*{
+      question: "String values must be enclosed within_____ when being assigned to variables."
+      correctAnswer: "quotes",
+      wrongAnswer1: "commas",
+      wrongAnswer2: "parentheses",
+      wrongAnswer3: "curly brackets",
+//This is really odd, correctAnswer gets an underlined red squiggle,
+//and makes the whole thing turn red,
+//so I've commented it out.
+  },*/
+  {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      correctAnswer: "console log",
+      wrongAnswer1: "JavaScript",
+      wrongAnswer2: "terminal/bash",
+      wrongAnswer3: "for loops",
+  }
+
+];
   //If you can, think about changing the position of the correctAnswer
   //each time
-];
 
 function renderLastRegistered() {
   //Retrieves all previous scores and user initials
@@ -53,7 +78,7 @@ function renderLastRegistered() {
   }
   else {
     var listItem = document.createElement("li");
-    listItem.textContent = prevInitials + " " + prevScores;
+    listItem.textContent = (prevInitials + " " + prevScores);
     listEl.appendChild(listItem);
   }
 }
@@ -68,6 +93,8 @@ You have made userLoss subtract 10 from it.
 }
 
 function startGame() {
+  //test
+  console.log("Has the event listener worked?");//not working
     //starts timer and everything else in timer function
     timer();
     //default userWin = false, not sure if necessary or desirable
@@ -81,15 +108,25 @@ function playGame() {
     formEl.appendChild(form);
 
     for (i = 0; i < questions.length; i++) {
-    //sets first question
+    //sets questions bvy looping through
     questionEl = questions[i].question;
-    //create two options for inside the form, give them content, append
+    //create 4 options for inside the form, give them content, append
     var choice1 = document.createElement("option");
+    var choice2 = document.createElement("option");
+    var choice3 = document.createElement("option");
+    var choice4 = document.createElement("options");
+    /*Need to change from option drop down to buttons.
+    Don't know how. Buttons have input with type AND label with text.
+    Create input with type radio AND label with text?
+    */
     choice1.textContent = questions[i].correctAnswer;
     form.appendChild(choice1);
-    var choice2 = document.createElement("option");
-    choice2.textContent = questions[i].wrongAnswer;
+    choice2.textContent = questions[i].wrongAnswer1;
     form.appendChild(choice2);
+    choice3.textContent = questions[i].wrongAnswer2;
+    form.appendChild(choice3);
+    choice4.textContent = questions[i].wrongAnswer3;
+    form.appendChild(choice4);
     /*addEventListener that tracks the user's choice
     and updates the variable to be used in the if statement.*/
     userChoice.addEventListener('click', function (event) {
@@ -97,13 +134,14 @@ function playGame() {
       chosenAnswer = userChoice.option;/*wanted to write value
       here, but the computer would let me, kept subbing in 
       ariaValueMax...?! So I tried option instead but this is a guess.
+      Might be different when changing to radio buttons anyway.
       */
     });
     if (chosenAnswer === questions[i].correctAnswer) {
       userWins();
     }
     else {
-      userLoss();//function not yet created
+      userLoss();
     }
 }}
 
@@ -122,8 +160,9 @@ function userLoss() {
 
 //update from last played
 renderLastRegistered();
+
 //start game when user clicks on button
-start.addEventListener ('click', startGame);//not yet working, don't understand why yet
+start.addEventListener('click', startGame);//not yet working, don't understand why
 
 /*Make sure to set up any variables in here 
 as you go along and think about the order.
